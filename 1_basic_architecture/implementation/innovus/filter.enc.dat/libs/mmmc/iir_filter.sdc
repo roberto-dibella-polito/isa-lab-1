c2256f62a07e05d0dc3e3c458e94648e2bd22dcb
Robbozz <roberto.dibb@gmail.com>
2020-11-22 18:40:10 +0100

    Basic architecure place & route, and post simulation, power record

---
+++ 1_basic_architecture/implementation/innovus/filter.enc.dat/libs/mmmc/iir_filter.sdc
@@ -0,0 +1,1 @@
+/home/isa32/DELIVER/isa-lab-1/1_basic_architecture/implementation/netlist/iir_filter.sdc
---
+++ 1_basic_architecture/implementation/sim/post_route_power.tcl
@@ -0,0 +1,23 @@
+#record switching activity
+vcom -93 -work ./work ../src/constants.vhd
+vcom -93 -work ./work ../src/clk_gen.vhd
+vcom -93 -work ./work ../src/data_maker.vhd
+vcom -93 -work ./work ../src/data_sink_syn.vhd
+
+#To compile the verilog type:
+vlog -work ./work ../netlist/iir_filter.v
+vlog -work ./work ../tb/tb_iir.v
+
+
+vsim -L /software/dk/nangate45/verilog/msim6.2g work.tb_iir
+
+#Linking the delay file .sdf
+vsim -L /software/dk/nangate45/verilog/msim6.2g -sdftyp /tb_iir/DUT=../netlist/iir_filter.sdf work.tb_iir
+
+vcd file ../vcd/design.vcd
+
+vcd add /tb_iir/DUT/*
+
+view wave
+add wave *
+run 2500 ns
